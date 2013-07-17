c25e70f8d6b77ae2631d1b96bc3c5cb007d31e31
Thijs Brobbel <thijsbrobbel@gmail.com>
2013-07-17 14:38:34 +0200

    Adds test to reproduce the bug.

--- Delta.Iris.ReproduceCloneBug/ReproduceCloneBugExtension.cs
+++ Delta.Iris.ReproduceCloneBug/ReproduceCloneBugExtension.cs
@@ -2,47 +2,74 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.Reflection;
+
+using log4net;
+
+using ESRI.ArcGIS.Desktop.AddIns;
+
+using AedSicad.UT.Gis.CoreRuntime;
+using AedSicad.UT.Foundation;
 
 namespace Delta.Iris.ReproduceCloneBug
 {
-    public class ReproduceCloneBugExtension : ESRI.ArcGIS.Desktop.AddIns.Extension
+    public class ReproduceCloneBugExtension : Extension, IUtilityApplicationShellHook
     {
-        public ReproduceCloneBugExtension()
-        {
-        }
+        static readonly log4net.ILog Log =
+            LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public ReproduceCloneBugExtension() { }
 
         protected override void OnStartup()
         {
-            //
-            // TODO: Uncomment to start listening to document events
-            //
-            // WireDocumentEvents();
+            UTRuntimeEnvironment.Instance.WorkspaceChanged += HookupToDatasource;
         }
 
-        private void WireDocumentEvents()
+        void HookupToDatasource(object sender, WorkspaceChangedEventArgs e)
         {
-            //
-            // TODO: Sample document event wiring code. Change as needed
-            //
-
-            // Named event handler
-            ArcMap.Events.NewDocument += delegate() { ArcMap_NewDocument(); };
-
-            // Anonymous event handler
-            ArcMap.Events.BeforeCloseDocument += delegate()
+            var shell = GetShell();
+            if (shell == null)
             {
-                // Return true to stop document from closing
-                ESRI.ArcGIS.Framework.IMessageDialog msgBox = new ESRI.ArcGIS.Framework.MessageDialogClass();
-                return msgBox.DoModal("BeforeCloseDocument Event", "Abort closing?", "Yes", "No", ArcMap.Application.hWnd);
-            };
-
+                Log.Error("Failed to retrieve the UT shell.");
+                return;
+            }
+            GetHooked(shell);
         }
 
-        void ArcMap_NewDocument()
+        UtilityApplicationShell GetShell()
         {
-            // TODO: Handle new document event
+            return UTRuntimeEnvironment.Instance.Shell;
         }
 
-    }
 
+        public void GetHooked(UtilityApplicationShell shell)
+        {
+            shell.Datasource.AssetCreated += Datasource_AssetCreated;
+        }
+
+        void Datasource_AssetCreated(object sender, AssetEventArgs e)
+        {
+            var asset = e.Asset;
+            Log.InfoFormat("Datasource_AssetCreated: {0}", FormatAsset(asset));
+            asset.Cloned += asset_Cloned;
+        }
+
+        string FormatAsset(Asset asset)
+        {
+            return string.Format("{0}[OBJ_ID={1}]", asset.UtilityClass, asset.UTObjId);
+        }
+
+        void asset_Cloned(object sender, AssetCloneEventArgs e)
+        {
+            var originalAsset = e.Asset;
+            var clonedAsset = e.ClonedAsset;
+            Log.InfoFormat("asset_Cloned: original asset: {0}, cloned asset: {1}",
+                FormatAsset(originalAsset), FormatAsset(clonedAsset));
+        }
+
+        public string ImplementationInfo
+        {
+            get { return "Delta.Iris.ReproduceCloneBug"; }
+        }
+    }
 }
